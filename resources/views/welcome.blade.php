8b7e999ad80c951eb48b3dac8374441ef1d393d0
Heroku <jicahe9967@verumst.com>
2021-04-19 00:12:07 -0500

    ajustes de rutas api en nginx server 2.6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <link rel="icon" href="/favicon.ico">
+    <link rel="icon" href="{{ asset('favicon.ico')}}">
     <title>gestiontareas</title>
     <link href="{{ asset('js/about.5f814e71.js')}}" rel="prefetch">
     <link href="{{ asset('css/app.dd315a8f.css')}}" rel="preload" as="style">
     <link href="{{ asset('css/chunk-vendors.d01af9ac.css')}}" rel="preload" as="style">
-    <link href="{{ asset('js/app.e58be98b.js')}}" rel="preload" as="script">
+    <link href="{{ asset('js/app.0c3b7be8.js')}}" rel="preload" as="script">
     <link href="{{ asset('js/chunk-vendors.d80a27eb.js')}}" rel="preload" as="script">
     <link href="{{ asset('css/chunk-vendors.d01af9ac.css')}}" rel="stylesheet">
     <link href="{{ asset('css/app.dd315a8f.css')}}" rel="stylesheet">
@@ -20,7 +19,7 @@
 <body><noscript><strong>We're sorry but gestiontareas doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
     <div id="app"></div>
     <script src="{{ asset('js/chunk-vendors.d80a27eb.js')}}"></script>
-    <script src="{{ asset('js/app.e58be98b.js')}}"></script>
+    <script src="{{ asset('js/app.0c3b7be8.js')}}"></script>
 </body>
 
 </html>
