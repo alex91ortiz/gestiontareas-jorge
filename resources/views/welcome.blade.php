480a22f5ea43081eb36a96401892f6f9a99e6a79
Heroku <jicahe9967@verumst.com>
2021-04-19 00:01:45 -0500

    ajustes de rutas api en nginx server 2.4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,23 +5,21 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="icon" href="/favicon.ico">
     <title>gestiontareas</title>
-    <link href="{{ asset('js/about.5f814e71.js') }}" rel="prefetch">
-    <link href="{{ asset('css/app.dd315a8f.css') }}" rel="preload" as="style">
-    <link href="{{ asset('css/chunk-vendors.d01af9ac.css') }}" rel="preload" as="style">
-    <link href="{{ asset('js/app.5493d88c.js') }}" rel="preload" as="script">
-    <link href="{{ asset('js/chunk-vendors.d80a27eb.js') }}" rel="preload" as="script">
-    <link href="{{ asset('css/chunk-vendors.d01af9ac.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.dd315a8f.css') }}" rel="stylesheet">
-    
+    <link href="{{ asset('js/about.5f814e71.js')}}" rel="prefetch">
+    <link href="{{ asset('css/app.dd315a8f.css')}}" rel="preload" as="style">
+    <link href="{{ asset('css/chunk-vendors.d01af9ac.css')}}" rel="preload" as="style">
+    <link href="{{ asset('js/app.e58be98b.js')}}" rel="preload" as="script">
+    <link href="{{ asset('js/chunk-vendors.d80a27eb.js')}}" rel="preload" as="script">
+    <link href="{{ asset('css/chunk-vendors.d01af9ac.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/app.dd315a8f.css')}}" rel="stylesheet">
 </head>
 
 <body><noscript><strong>We're sorry but gestiontareas doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
     <div id="app"></div>
-    <script src="{{ asset('js/chunk-vendors.d80a27eb.js') }}"></script>
-    <script src="{{ asset('js/app.5493d88c.js')}}"></script>
+    <script src="{{ asset('js/chunk-vendors.d80a27eb.js')}}"></script>
+    <script src="{{ asset('js/app.e58be98b.js')}}"></script>
 </body>
 
 </html>
